c36ea3196d1977e3b0e2463e2819411c4be0d651
Junnygram <you@example.com>
2024-04-24 14:39:00 +0100

    chore: pwa setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-between p-24">
+      <div className="flex items-center justify-center text-4xl">
+        PWA with nextjs
+      </div>
+    </main>
+  );
+}
