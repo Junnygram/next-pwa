7eab1e8fcbc491a1551f211d1fe13814705028d8
Junnygram <you@example.com>
2024-04-24 14:57:16 +0100

    ft: checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-between p-24">
-      <div className="flex items-center justify-center text-4xl">
+      <div className="flex items-center justify-center text-xl">
         PWA with nextjs
       </div>
     </main>
